d530b343cc3a1c4ba2da9a3687e0c0d225a60dc9
dima kiselev <d.s.matroskin@gmail.com>
2019-01-21 14:11:28 +0300

    feat: add Foundation

--- src/js/vendor.js
+++ src/js/vendor.js
@@ -1,10 +1,15 @@
 import '@babel/polyfill';
 import svg4everybody from 'svg4everybody';
 import $ from 'jquery';
+import Foundation from 'foundation-sites';
 
 svg4everybody();
 
 window.$ = $;
 window.jQuery = $;
+
+Foundation.addToJquery($);
+
+$(document).foundation();
 
 require('ninelines-ua-parser');
